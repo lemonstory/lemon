fc921a1b3a0bf3744e1d3b9fc8499cb3381577dc
huqq820 <will8436992@sina.com>
2015-08-10 18:45:09 +0800

    add file

---
+++ libs/alioss/conf.inc.php
@@ -0,0 +1,21 @@
+<?php
+//个人测试
+//ACCESS_ID
+define('OSS_ACCESS_ID', 'xxx');
+
+//ACCESS_KEY
+define('OSS_ACCESS_KEY', 'xxx');
+
+//是否记录日志
+define('ALI_LOG', false);
+
+//自定义日志路径，如果没有设置，则使用系统默认路径，在./logs/
+define('ALI_LOG_PATH','/alidata/log/php/alioss.upload.log');
+
+//是否显示LOG输出
+define('ALI_DISPLAY_LOG', FALSE);
+
+//语言版本设置
+define('ALI_LANG', 'zh');
+
+
