eb7b1dbb92f8012c66d9492c307932896599d838
huqq820 <will8436992@sina.com>
2015-08-11 13:26:56 +0800

    测试

--- userinfo/text.php
+++ userinfo/text.php
@@ -1,1 +1,27 @@
-test
+<?php
+include_once '../controller.php';
+class text extends controller 
+{
+    function action() {
+        // 获取当前登录用户uid
+        $uid = $this->getUid();
+        if ($uid == 0) {
+            // 返回错误json
+            $this->showErrorJson(ErrorConf::noLogin());
+        }
+        
+        // 接收参数
+        $nickname = trim($this->getRequest('nickname'));
+        if ($nickname == "") {
+            $this->showErrorJson(ErrorConf::nickNameisEmpty());
+        }
+        
+        // 调用类的方法，类方法在model中，底层数据操作在方法中实现
+        $aliossobj = new AliOss();
+        $res = $aliossobj->xxx($nickname);
+        
+        // 返回成功json
+        $this->showSuccJson();
+    }
+}
+new text();
