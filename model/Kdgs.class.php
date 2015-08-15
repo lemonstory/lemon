c4cc93ab0e1abc9b8a662184b0bba6f218aca326
microsnow <weixue108@live.cn>
2015-08-16 01:45:29 +0800

    口袋故事 采集

--- model/Kdgs.class.php
+++ model/Kdgs.class.php
@@ -12,7 +12,7 @@ class Kdgs extends Http
 	/**
 	 * 获取顶级分类
 	 */
-	public static function get_parent_category($url)
+	public function get_parent_category($url)
 	{
 		$content = parent::get($url);
 
@@ -38,10 +38,11 @@ class Kdgs extends Http
 			$cover = http::sub_data($v, 'background-image:url(', ')');
 			$title = http::sub_data($v, '<span>', '</span>');
 			if ($title && $url) {
-				$r[$k]['url'] = $url;
+				$r[$k]['link_url'] = "http://m.idaddy.cn".htmlspecialchars_decode($url);
 				$r[$k]['title'] = $title;
 				$r[$k]['cover'] = $cover;
-				$r[$k]['s_cat_id'] = http::sub_data($url, 'parentId='); // parent_source_category_id
+				$r[$k]['s_id'] = http::sub_data($url, 'parentId=', '&'); // parent_source_category_id
+				$r[$k]['s_p_id'] = 0;
 			}
 		}
 
@@ -51,7 +52,7 @@ class Kdgs extends Http
 	/**
 	 * 获取二级分类
 	 */
-	public static function get_children_category_list($url)
+	public function get_children_category_list($url)
 	{
 		$content = parent::get($url);
 
@@ -75,13 +76,14 @@ class Kdgs extends Http
 
 		foreach($result[0] as $k => $v) {
 			$url   = http::sub_data($v, 'href="', '"');
-			$cover = http::sub_data($v, 'background-image:url(', ')');
+			$cover = http::sub_data($v, 'src="', '"');
 			$title = http::sub_data($v, '<span>', '</span>');
 			if ($title && $url) {
-				$r[$k]['url'] = $url;
+				$r[$k]['link_url'] = 'http://m.idaddy.cn'.htmlspecialchars_decode($url);
 				$r[$k]['title'] = $title;
 				$r[$k]['cover'] = $cover;
-				$r[$k]['p_s_cat_id'] = http::sub_data($url, 'term_taxonomy_id='); // parent_source_category_id
+				$r[$k]['s_p_id'] = http::sub_data($url, 'term_taxonomy_id=', '&'); // parent_source_category_id
+				$r[$k]['s_id']   = 0;
 			}
 		}
 		return $r;
@@ -90,10 +92,10 @@ class Kdgs extends Http
 
 	// 获取子分类的专辑列表
 	// term_taxonomy_id 为该网站解析的ID
-	public static function get_children_category_album_list($term_taxonomy_id = 0, $page = 1)
+	public function get_children_category_album_list($term_taxonomy_id = 0, $page = 1)
 	{
 		$r   = array();
-		$url = "http://m.idaddy.cn/m.php?etr=spadmin&mod=freeAudio&do=list&term_taxonomy_id={$term_taxonomy_id}&cat_id=0&keyword=&spId=0143&page={$page}&rand=".mt_rand(100,400);
+		$url = "http://m.idaddy.cn/m.php?etr=spadmin&mod=freeAudio&do=list&term_taxonomy_id={$term_taxonomy_id}&cat_id=0&keyword=&spId=0&page={$page}&rand=".mt_rand(100,400);
 
 		if (!$term_taxonomy_id) {
 			return array();
@@ -129,15 +131,30 @@ class Kdgs extends Http
 		foreach($result[0] as $k => $v) {
 
 			$url   = http::sub_data($v, 'href="', '"');
+			if (!strstr($url, 'http')) {
+				$url = 'http://m.idaddy.cn'.$url;
+			}
 			$cover = http::sub_data($v, 'src="', '"');
 			$title = http::sub_data($v, '<strong>', '</strong>');
 			if ($title && $url) {
-				$r[$k]['url'] = 'http://m.idaddy.cn'.$url;
-				$r[$k]['title'] = $title;
-				$r[$k]['cover'] = $cover;
+				$r[$k]['url']     = $url;
+				$r[$k]['title']   = $title;
+				$r[$k]['cover']   = $cover;
+				$r[$k]['min_age'] = 0;
+				$r[$k]['max_age'] = 0;
 				if (strstr($v, 'P')) {
-					$r[$k]['age'] = (int)http::sub_data($v, 'P-', '<');
+					$r[$k]['min_age'] = (int)http::sub_data($v, 'P-', '<');
 				} else {
+					$age = (int)http::sub_data($v, '<td style=" padding:0 0 0 10px;">', '<');
+					if (strstr($age, '-')) {
+						$tmp = explode("-", $age);
+						if (isset($tmp[1])) {
+							$r[$k]['max_age'] = $tmp[1];
+						}
+						if (isset($tmp[0])) {
+							$r[$k]['min_age'] = $tmp[0];
+						}
+					}
 					$r[$k]['age'] = (int)http::sub_data($v, '<td style=" padding:0 0 0 10px;">', '<');
 				}
 			}
@@ -146,7 +163,7 @@ class Kdgs extends Http
 	}
 
 	// 获取故事列表
-	public static function get_album_story_list($url = '')
+	public function get_album_story_list($url = '')
 	{
 		if (!$url) {
 			return array();
@@ -170,12 +187,12 @@ class Kdgs extends Http
 		foreach ($title_list as $k => $v) {
 			$title = http::sub_data($v, '>', '<');
 			$title = http::remove_n($title);
-			$audio_src = http::sub_data($audio_list[$k], "src='", "'");
-			if ($title && $audio_src) {
+			$source_audio_url = http::sub_data($audio_list[$k], "src='", "'");
+			if ($title && $source_audio_url) {
 				$r[$k]['title'] = $title;
 				$r[$k]['intro'] = $intro;
 				$r[$k]['cover'] = $cover;
-				$r[$k]['audio_src'] = $audio_src;
+				$r[$k]['source_audio_url'] = $source_audio_url;
 			}
 		}
 		return $r;
