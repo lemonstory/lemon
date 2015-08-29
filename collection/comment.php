04d6b9d5d44fa3e684359a7b202a60041174ec9e
microsnow <weixue108@live.cn>
2015-08-29 16:15:39 +0800

    获取tag

--- collection/comment.php
+++ collection/comment.php
@@ -53,5 +53,60 @@ class comment extends controller
 
         $comment_list = Http::ajax_get($url_page);
     }
+
+    /**
+     * 获取tag
+     */
+    private function get_tag_from_dangda()
+    {
+        $content = Http::sub_data($url);
+
+        $content =  @iconv('GBK', 'UTF-8', $content);
+        $content = Http::sub_data($content, '<div class="show_info_left">所属分类</div>', '</div>');
+        $content = htmlspecialchars_decode($content);
+        $content = str_replace("&nbsp", '', $content);
+        $r = explode("br", $content);
+
+        $tag_list = array();
+
+        foreach($r as $k => $v) {
+            preg_match_all('/<a[\S|\s].*?a>/', $v, $result);
+            foreach($result[0] as $k2 => $v2) {
+                $tag_list[$k][] = Http::sub_data($v2, '>', '<');
+            }
+        }
+
+        var_dump($tag_list);
+
+        /************
+        array(2) {
+          [0]=>
+          array(4) {
+            [0]=>
+            string(6) "图书"
+            [1]=>
+            string(6) "童书"
+            [2]=>
+            string(15) "平装图画书"
+            [3]=>
+            string(6) "欧美"
+          }
+          [1]=>
+          array(4) {
+            [0]=>
+            string(6) "图书"
+            [1]=>
+            string(6) "童书"
+            [2]=>
+            string(6) "3-6岁"
+            [3]=>
+            string(23) "卡通/动漫/图画书"
+          }
+        }
+
+        ************/
+
+
+    }
 }
 new comment();
