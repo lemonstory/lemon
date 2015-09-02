29bf7a7fb4e148f3bdacf00f9b089a116d7804a8
microsnow <weixue108@live.cn>
2015-09-02 10:50:40 +0800

    test

---
+++ album/test.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once '../controller.php';
+class test extends controller
+{
+    function action() {
+    	$album = new Album();
+        $album_list = $album->getListByIds(array(1,2,3));
+        var_dump($album_list);
+    }
+}
+new test();
+
