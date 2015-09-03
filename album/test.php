a58851ba2dccd610c78d10685c2da9d5d2426fdf
microsnow <weixue108@live.cn>
2015-09-03 16:33:10 +0800

    test

--- album/test.php
+++ album/test.php
@@ -5,7 +5,7 @@ class test extends controller
 {
     function action() {
     	$album = new Album();
-        $album_list = $album->getListByIds(array(1,2,3));
+        $album_list = $album->getListByIds(array(1,2,3), 1);
         var_dump($album_list);
     }
 }
