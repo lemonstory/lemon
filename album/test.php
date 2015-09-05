9180f6c11b3da0096d1ffb7d857b6f1c77c314c1
microsnow <weixue108@live.cn>
2015-09-05 19:25:49 +0800

    test

--- album/test.php
+++ album/test.php
@@ -4,9 +4,50 @@ include_once '../controller.php';
 class test extends controller
 {
     function action() {
+    	// $album = new Album();
+     //    $album_list = $album->getListByIds(array(1,2,3), 1);
+     //    var_dump($album_list);
     	$album = new Album();
-        $album_list = $album->getListByIds(array(1,2,3), 1);
-        var_dump($album_list);
+    	$album_list = $album->get_list("`id` > 0");
+    	$p = '';
+    	$s = '';
+    	foreach($album_list as $k => $v) {
+    		// 1 0~2
+    		// 2 3~6
+    		// 3 7~10
+    		$age_type = 0;
+    		if (strstr($v['age_str'], 'P')) {
+    			if ($v['age_str'] == 'P+') {
+    				$age_type = 1;
+    			} else if (strstr($v['age_str'], '-')) {
+    				$tmp = explode('-', $v['age_str']);
+    				if (isset($tmp[1])) {
+    					if ($tmp[1] <= 2) {
+    						$age_type = 1;
+    					} else if ($tmp[1] <= 6 ) {
+    						$age_type = 2;
+    					} else if ($tmp[1] <= 10 ) {
+    						$age_type = 3;
+    					}
+    				}
+    			}
+    		} else if (strstr($v['age_str'], '岁')) {
+    			$str = str_replace('岁', '', $v['age_str']);
+    			$tmp = explode('-', $str);
+    			if (isset($tmp[1])) {
+					if ($tmp[1] <= 2) {
+						$age_type = 1;
+					} else if ($tmp[1] <= 6 ) {
+						$age_type = 2;
+					} else if ($tmp[1] <= 10 ) {
+						$age_type = 3;
+					}
+				}
+    		} else {
+    			echo $v['age_str'];
+    		}
+    		$album->update(array('age_type' => $age_type), "`id`={$v['id']}");
+    	}
     }
 }
 new test();
