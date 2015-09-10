9002a47aba7e3673004ff8bfe6c1b06163256fa8
microsnow <weixue108@live.cn>
2015-09-10 20:24:04 +0800

    // 长度限制

--- album/all.php
+++ album/all.php
@@ -7,6 +7,10 @@ class all extends controller
     	$direction = $this->getRequest("direction", "down");
         $startid = $this->getRequest("startid", 0);
         $len = $this->getRequest("len", 0);
+        // 长度限制
+        if ($len > 50) {
+            $len = 50;
+        }
 
         $album = new Album();
 
