f1ed8482a4f8610b832cacaea33d11b4dae045b2
microsnow <weixue108@live.cn>
2015-09-21 17:44:14 +0800

    专辑详情 故事详情修改

--- album/info.php
+++ album/info.php
@@ -5,11 +5,15 @@ class info extends controller
 {
     function action() {
     	$result  = array();
-        $album   = new Album();
-        $story   = new Story();
-        $comment = new Comment();
-        $useralbumlog = new UserAlbumLog();
+        $album            = new Album();
+        $story            = new Story();
+        $comment          = new Comment();
+        $useralbumlog     = new UserAlbumLog();
         $useralbumlastlog = new UserAlbumLastlog();
+        $fav              = new Fav();
+        $listenobj        = new Listen();
+
+
 
         $uid     = $this->getUid();
 
@@ -26,11 +30,28 @@ class info extends controller
             $playinfo = [];
         }
         $result['playinfo'] = $playinfo;
+        // 是否收藏
+        $favinfo = $fav->getUserFavInfoByAlbumId($uid, $album_id);
+        if ($favinfo) {
+            $result['fav'] = 1;
+        } else {
+            $result['fav'] = 0;
+        }
+        // 收听数量
+        $albumlistennum = $listenobj->getAlbumListenNum($album_id);
+        if ($albumlistennum) {
+            $result['albumlistennum'] = $albumlistennum[$album_id];
+        } else {
+            $result['albumlistennum'] = 0;
+        }
+        
 
         $story_list = $story->get_list("`album_id`={$album_id}");
         foreach ($story_list as $k => $v) {
         	$result['storylist'][] = $story->format_to_api($v);
         }
+        // 评论数量
+        $result['commentcount'] = $comment->get_total("`albumid`={$album_id}");
         $result['commentlist'] = $comment->get_comment_list();
 
         // 返回成功json
--- album/story_info.php
+++ album/story_info.php
@@ -5,13 +5,22 @@ class story_info extends controller
 {
     function action() {
 
+    	$fav   = new Fav();
         $story = new Story();
 
-        $story_id = (int)$this->getRequest("storyid", "1");;
+        $story_id   = (int)$this->getRequest("storyid", "1");;
 
         $story_info = $story->get_story_info($story_id);
 
 		$story_info = $story->format_to_api($story_info);
+
+		// 专辑收藏总数
+		$fav_list   = $fav->getAlbumFavCount($story_info['albumid']);
+		if ($fav_list) {
+			$story_info['albumfavcount'] = $fav_list[$story_info['albumid']];
+		} else {
+			$story_info['albumfavcount'] = 0;
+		}
 
         // 返回成功json
         $this->showSuccJson($story_info);
