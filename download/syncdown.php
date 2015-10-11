a93b7d8e5587af174444fa1c5983472407459f49
huqiangqiang <will8436992@sina.com>
2015-10-11 13:18:55 +0800

    add syncdown

--- download/syncdown.php
+++ download/syncdown.php
@@ -23,18 +23,53 @@ class syncdown extends controller
             $this->showErrorJson(ErrorConf::userImsiIdError());
         }
         
+        $albumids = array();
+        $storyids = array();
+    	foreach ($data as $value) {
+        	if (empty($value['clientid']) || empty($value['albumid']) || empty($value['storyid']) || empty($value['status'])) {
+        		continue;
+        	}
+        	$albumids[] = $value['albumid'];
+        	$storyids[] = $value['storyid'];
+    	}
+    	if (empty($albumids) || empty($storyids)) {
+    		$this->showErrorJson(ErrorConf::paramError());
+    	}
+    	$albumids = array_unique($albumids);
+    	$storyids = array_unique($storyids);
+    	
+    	$albumobj = new Album();
+    	$albumlist = $albumobj->getListByIds($albumids);
+    	if (empty($albumlist)) {
+    		$this->showErrorJson(ErrorConf::albumInfoIsEmpty());
+    	}
+    	$storyobj = new Story();
+    	$storylist = $storyobj->getListByIds($storyids);
+    	if (empty($storylist)) {
+    		$this->showErrorJson(ErrorConf::storyInfoIsEmpty());
+    	}
+        
         $downloadobj = new DownLoad();
         $actionlogobj = new ActionLog();
+        $successdata = array();
         foreach ($data as $value) {
-        	if (empty($value['albumid']) || empty($value['storyid']) || empty($value['status'])) {
+        	$tmplist = array('clientid' => $value['clientid'], 'result' => false);
+        	if (empty($value['clientid']) || empty($value['albumid']) || empty($value['storyid']) || empty($value['status'])) {
+        		continue;
+        	}
+        	if (empty($albumlist[$value['albumid']]) || empty($storylist[$value['storyid']])) {
         		continue;
         	}
         	$res = $downloadobj->addDownLoadStoryInfo($uimid, $value['albumid'], $value['storyid'], $value['status']);
         	if ($res == true) {
+        		$tmplist = array('clientid' => $value['clientid'], 'result' => true);
 		        MnsQueueManager::pushActionLogQueue($uimid, $uid, $actionlogobj->ACTION_TYPE_DOWNLOAD_STORY);
         	}
+        	
+        	$successdata[] = $tmplist;
         }
         
+        $this->showSuccJson($successdata);
     }
     
 }
