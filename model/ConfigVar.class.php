aa4ecdd04bb8dfe2ecc80e29cbce4acef1da5e86
huqiangqiang <will8436992@sina.com>
2015-11-02 16:10:18 +0800

    edit agetypelist

--- model/ConfigVar.class.php
+++ model/ConfigVar.class.php
@@ -5,6 +5,12 @@ class ConfigVar
     public $AGE_TYPE_SECOND = 2; // 3-6岁
     public $AGE_TYPE_THIRD = 3; // 7-10岁
     public $AGE_TYPE_LIST = array(1, 2, 3);
+    public $AGE_TYPE_NAME_LIST = array(
+            "0" => "全部",
+            "1" => "0-2岁",
+            "2" => "3-6岁",
+            "3" => "7-10岁"
+            );
     
     public $RECOMMEND_STATUS_ONLIINE = 1; // 推荐上线状态
     public $RECOMMEND_STATUS_OFFLINE = 2; // 推荐下线状态
