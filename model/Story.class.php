10368ba3466bd503936ce9438761b410680888a7
microsnow <weixue108@live.cn>
2015-11-07 12:34:31 +0800

    专辑 故事 排序

--- model/Story.class.php
+++ model/Story.class.php
@@ -306,7 +306,7 @@ class Story extends ModelBase
         if ($redisData) {
             $new_list = json_decode($redisData, true);
         } else {
-            $story_list = $this->get_list("`album_id`='{$album_id}' and status=1");
+            $story_list = $this->get_list("`album_id`='{$album_id}' and status=1", '', '', ' ORDER BY `id` DESC,`view_order` ASC ');
             foreach ($story_list as $k => $v) {
                 $new_list[] = $this->format_to_api($v);
             }
