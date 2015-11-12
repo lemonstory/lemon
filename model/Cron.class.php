9ca9b56801fee679209ba2059c55d257dfcff853
huqiangqiang <will8436992@sina.com>
2015-11-12 19:15:42 +0800

    edit 添加入库后的清空队列

--- model/Cron.class.php
+++ model/Cron.class.php
@@ -8,7 +8,7 @@ class Cron extends ModelBase
     /**
      * cron进程执行
      * 添加同龄在听到推荐表
-     * 按照年龄段，将用户收听次数最多的专辑展示在首页
+     * 每天执行一次，按照年龄段，将用户收听次数最多的专辑添加到同龄在听推荐表，并清空队列
      * @param I $babyagetype	当前用户的宝宝年龄段
      * @param I $start			列表开始位置
      * @param I $len			列表长度
@@ -17,7 +17,7 @@ class Cron extends ModelBase
     public function cronSaveSameAgeToDb()
     {
         $start = 0;
-        $len = 100;
+        $len = 200;
         $addtime = date("Y-m-d H:i:s");
         
         $redisobj = AliRedisConnecter::connRedis($this->RANK_INSTANCE);
@@ -40,6 +40,9 @@ class Cron extends ModelBase
                     continue;
                 }
             }
+            
+            // 删除指定年龄段的队列
+            $redisobj->delete($albumkey);
         }
     
         return true;
