4d0f2170a37d5f424ee03ba3cc9bb3231543caec
huqiangqiang <will8436992@sina.com>
2015-12-03 12:20:02 +0800

    add httpcacheconf.php

---
+++ config/httpcacheconf.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * http cache 模块配置
+ * 规则：
+ * "module_action" => array(
+ *     "action" => "module_action",
+ *     "cachetime" => "缓存时间",
+ *     "params" => array("参数1", "参数2")
+ * )
+ */
+$_SERVER['http_cache_conf'] = array(
+        // 首页
+        "default_index" => array(
+                "action" => "default_index",
+                "cachetime" => 60,
+                "params" => array()
+        ),
+        
+        // listen
+        "liste_getlistenlist" => array(
+                "action" => "liste_getlistenlist",
+                "cachetime" => 60,
+                "params" => array("direction", "startid", "len")
+        ),
+);
