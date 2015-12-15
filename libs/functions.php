7c04397686c06b4f1bffd2409e8e68afcf08389f
huqiangqiang <will8436992@sina.com>
2015-12-15 17:16:32 +0800

    edit 增加imsi对32位的ios支持

--- libs/functions.php
+++ libs/functions.php
@@ -77,8 +77,9 @@ function getImsi()
     }
     // 设备唯一标识imsi
     $imsi = $agentArr[1];
-    //Mi 3C imsi只有14位
-    if (strlen($imsi) != 15 && strlen($imsi) != 14) {
+    $imsilen = strlen($imsi);
+    //Mi 3C imsi只有14位, ios 32位，其余15位
+    if (!in_array($imsilen, array(14, 15, 32))) {
         return '';
     }
     return $imsi;
