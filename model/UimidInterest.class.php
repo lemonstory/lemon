2c5bf4c723bca91e0bd6c3fba79276dc24c4589a
huqiangqiang <will8436992@sina.com>
2016-02-24 17:24:57 +0800

    edit 添加设备感兴趣的专辑进程

--- model/UimidInterest.class.php
+++ model/UimidInterest.class.php
@@ -1,22 +1,72 @@
 <?php
 /*
- * 用户设备，感兴趣的专辑
- * 从感兴趣的专辑，所属的所有标签中，筛选出公共标签
+ * uimid_interest_tag    设备感兴趣的标签
+ * uimid_interest_album  从设备感兴趣的标签中，筛选出标签相似度【行为出现频率num】高的标签。再从这些标签中，按一定规则筛选出推荐给用户的专辑，并设置推荐度【recommendscore】
  */
 class UimidInterest extends ModelBase
 {
     public $UIMID_INTEREST_ALBUM_TABLE_NAME = 'uimid_interest_album';
     public $UIMID_INTEREST_TAG_TABLE_NAME = 'uimid_interest_tag';
     
+    
     /**
-     * 获取指定设备喜好的公共标签列表
+     * 获取指定时间内的记录
+     * @param S $starttime    开始时间，如2016-02-22 10:00:00
+     * @param S $endtime
+     * @return array
+     */
+    public function getUimidInterestTagListByTime($starttime, $endtime)
+    {
+        $db = DbConnecter::connectMysql($this->ANALYTICS_DB_INSTANCE);
+        $sql = "select * from {$this->UIMID_INTEREST_TAG_TABLE_NAME} where `addtime` >= ? and `addtime` <= ? order by `num` desc";
+        $st = $db->prepare ( $sql );
+        $st->execute (array($starttime, $endtime));
+        $dbData = $st->fetchAll(PDO::FETCH_ASSOC);
+        if (empty($dbData)) {
+            return array();
+        }
+        return $dbData;
+    }
+    
+    /**
+     * 获取指定设备喜好的标签列表
      * @param S $uimid
      * @return array
      */
-    public function getUimidInterestTagList($uimid, $len)
+    public function getUimidInterestTagListByUimid($uimid, $len)
     {
         $db = DbConnecter::connectMysql($this->ANALYTICS_DB_INSTANCE);
-        $sql = "select * from {$this->UIMID_INTEREST_TAG_TABLE_NAME} where `uimid` = ? order by id desc limit {$len}";
+        $sql = "select * from {$this->UIMID_INTEREST_TAG_TABLE_NAME} where `uimid` = ? order by `num` desc limit {$len}";
+        $st = $db->prepare ( $sql );
+        $st->execute (array($uimid));
+        $dbData = $st->fetchAll(PDO::FETCH_ASSOC);
+        if (empty($dbData)) {
+            return array();
+        }
+        return $dbData;
+    }
+    public function getUimidInterestTagListByTagid($tagid, $len)
+    {
+        $db = DbConnecter::connectMysql($this->ANALYTICS_DB_INSTANCE);
+        $sql = "select * from {$this->UIMID_INTEREST_TAG_TABLE_NAME} where `tagid` = ? order by `num` desc limit {$len}";
+        $st = $db->prepare ( $sql );
+        $st->execute (array($tagid));
+        $dbData = $st->fetchAll(PDO::FETCH_ASSOC);
+        if (empty($dbData)) {
+            return array();
+        }
+        return $dbData;
+    }
+    
+    /**
+     * 获取指定设备喜好的专辑列表
+     * @param S $uimid
+     * @return array
+     */
+    public function getUimidInterestAlbumListByUimid($uimid, $len)
+    {
+        $db = DbConnecter::connectMysql($this->ANALYTICS_DB_INSTANCE);
+        $sql = "select * from {$this->UIMID_INTEREST_ALBUM_TABLE_NAME} where `uimid` = ? order by `recommendscore` desc limit {$len}";
         $st = $db->prepare ( $sql );
         $st->execute (array($uimid));
         $dbData = $st->fetchAll(PDO::FETCH_ASSOC);
@@ -47,6 +97,26 @@ class UimidInterest extends ModelBase
         return true;
     }
     
+    /**
+     * 新增设备感兴趣的标签，或更新感兴趣的专辑，以及推荐值
+     * @param S $uimid
+     * @param I $albumid
+     * @return boolean
+     */
+    public function updateUimidInterestAlbum($uimid, $albumid)
+    {
+        if (empty($uimid) || empty($albumid)) {
+            return false;
+        }
+        $interestinfo = $this->getUimidInterestAlbumInfoDb($uimid, $albumid);
+        if (empty($interestinfo)) {
+            $this->addUimidInterestAlbumDb($uimid, $albumid);
+        } else {
+            $this->updateUimidInterestAlbumRecommendScoreDb($interestinfo['id']);
+        }
+        return true;
+    }
+    
     
     /**
      * 获取指定设备、标签的标签喜好记录
@@ -60,6 +130,19 @@ class UimidInterest extends ModelBase
         $sql = "select * from {$this->UIMID_INTEREST_TAG_TABLE_NAME} where `uimid` = ? and `tagid` = ?";
         $st = $db->prepare ( $sql );
         $st->execute (array($uimid, $tagid));
+        $dbData = $st->fetch(PDO::FETCH_ASSOC);
+        if (empty($dbData)) {
+            return array();
+        }
+        return $dbData;
+    }
+    
+    private function getUimidInterestAlbumInfoDb($uimid, $albumid)
+    {
+        $db = DbConnecter::connectMysql($this->ANALYTICS_DB_INSTANCE);
+        $sql = "select * from {$this->UIMID_INTEREST_ALBUM_TABLE_NAME} where `uimid` = ? and `albumid` = ?";
+        $st = $db->prepare ( $sql );
+        $st->execute (array($uimid, $albumid));
         $dbData = $st->fetch(PDO::FETCH_ASSOC);
         if (empty($dbData)) {
             return array();
@@ -90,11 +173,38 @@ class UimidInterest extends ModelBase
         }
         return true;
     }
+    
+    private function addUimidInterestAlbumDb($uimid, $albumid)
+    {
+        if (empty($uimid) || empty($albumid)) {
+            return false;
+        }
+        $addtime = date("Y-m-d H:i:s");
+        $db = DbConnecter::connectMysql($this->ANALYTICS_DB_INSTANCE);
+        $sql = "INSERT INTO `{$this->UIMID_INTEREST_ALBUM_TABLE_NAME}` (`uimid`, `albumid`, `recommendscore`) VALUES (?, ?, ?)";
+        $st = $db->prepare ( $sql );
+        $res = $st->execute (array($uimid, $albumid, 1));
+        if (empty($res)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     // 更新感兴趣的标签次数
     private function updateUimidInterestTagNumDb($id)
     {
         $db = DbConnecter::connectMysql($this->ANALYTICS_DB_INSTANCE);
         $sql = "UPDATE `{$this->UIMID_INTEREST_TAG_TABLE_NAME}` SET `num` = `num` + 1 WHERE `id` = ?";
+        $st = $db->prepare ( $sql );
+        $st->execute (array($id));
+        return true;
+    }
+    
+    private function updateUimidInterestAlbumRecommendScoreDb($id)
+    {
+        $db = DbConnecter::connectMysql($this->ANALYTICS_DB_INSTANCE);
+        $sql = "UPDATE `{$this->UIMID_INTEREST_ALBUM_TABLE_NAME}` SET `recommendscore` = `recommendscore` + 1 WHERE `id` = ?";
         $st = $db->prepare ( $sql );
         $st->execute (array($id));
         return true;
