d4e483de9e8af8e6d7928c65384568963f7d9c7c
jack02.zhang <jack02.zhang@vipshop.com>
2016-09-24 10:44:43 +0800

    全部分类接口

--- default/category.php
+++ default/category.php
@@ -5,5 +5,30 @@
  * Date: 2016/9/24
  * Time: 上午9:58
  */
-
-echo "test";
+include_once '../controller.php';
+class categorylist extends controller
+{
+    public function action()
+    {
+        $categoryObj = new Category();
+        $data = array();
+        //获取一级分类
+        $firstList = $categoryObj->get_list(' parent_id = 0 ');
+        $data['total'] = count($firstList);
+        foreach($firstList as $key=>$val){
+            $tmp['id'] = $val['id'];
+            $tmp['title'] = $val['title'];
+            //取二级分类
+            $secondList = $categoryObj->get_list(' parent_id = '.$val['id']);
+            $tmp['child_total'] = count($secondList);
+            foreach($secondList as $val1){
+                $tmp1['id'] = $val1['id'];
+                $tmp1['title'] = $val1['title'];
+                $tmp['child_items'][] = $tmp1;
+            }
+            $data['items'][] = $tmp;
+        }
+        echo json_encode($data);
+    }
+}
+new categorylist();
