e194a509e62620ebb3353a08debf8b335de7657b
jack02.zhang <jack02.zhang@vipshop.com>
2016-09-29 11:53:13 +0800

    热门播放列表

--- default/hot_album_list.php
+++ default/hot_album_list.php
@@ -13,10 +13,23 @@ class hotalbumlist extends controller
         $minAge = $this->getRequest('min_age', '0');
         $maxAge = $this->getRequest('max_age', '2');
         $startAlbumId = $this->getRequest('start_album_id', '');
-        $len = $this->getRequest('len', '');
+        $len = $this->getRequest('len', 10);
 
+        $where['min_age']= intval($minAge);
+        $where['max_age']= intval($maxAge);
 
-        $data = array('age_level'=>array(),'total'=>100,'items'=>array());
+        $albumTagObj = new AlbumTagRelation();
+        $albumTagList = $albumTagObj->getAlbumListByTagId($where,1,$len);
+        
+        //取专辑下面对应的故事
+        $storyObj = new Story();
+        foreach ($albumTagList as $key=>$val){
+            $storyList = $storyObj->get_filed_list('title,cover',' album_id='.$val['id'],'',4);
+            $val['items'] = $storyList;
+            $albumTagList[$key]=$val;
+        }
+
+        $data = array('age_level'=>array(),'total'=>100,'items'=>$albumTagList);
         $this->showSuccJson($data);
     }
 }
