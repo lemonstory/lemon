dc6cdbabe377066cbc3c08f1c386dd552b1f31a0
jack02.zhang <jack02.zhang@vipshop.com>
2016-10-18 20:00:16 +0800

    删除没有必要返回的字段  修改变量没有重置为空 造成很多tag的bug

--- default/v2.6/hot_album_list.php
+++ default/v2.6/hot_album_list.php
@@ -18,15 +18,14 @@ class hotalbumlist extends controller
 
         $aliossObj = new AliOss();
         $albumObj = new Album();
-        $select = 'a.id,a.title,a.intro,a.category_id,a.star_level,a.view_order,a.story_num,a.author,
-        a.age_str,a.status,a.add_time,a.update_time,a.cover,a.cover_time,a_t.albumfavnum as fav,a_t.albumlistennum as listen_num';
+        $select = 'a.id,a.title,a.intro,a.star_level,a.view_order,a.story_num,a.author,
+        a.age_str,a.cover,a.cover_time,a_t.albumfavnum as fav,a_t.albumlistennum as listen_num';
         $albumTagList = $albumObj->getAlbumListByAge($minAge,$maxAge, $startAlbumId,$select,1,$len);
         
         //取专辑下面对应的标签
         $tagnewobj = new TagNew();
         $tagInfoObj = new TagInfo();
         $recommendDescObj = new RecommendDesc();
-        $tagInfoList = array();
         foreach ($albumTagList as $key=>$val){
             // 获取推荐语
             $recommendList = $recommendDescObj->getAlbumRecommendDescList($val['id']);
@@ -34,8 +33,9 @@ class hotalbumlist extends controller
 
             $val['cover'] = $aliossObj->getImageUrlNg($aliossObj->IMAGE_TYPE_ALBUM, $val['cover'], 460, $val['cover_time']);
             $tagList = $tagnewobj->getAlbumTagRelationListByAlbumIds(array($val['id']));
+            $tagInfoList = array();
             foreach ($tagList[$val['id']] as $k=>$v){
-                $tagInfo = $tagInfoObj->get_info("id = ".$v['tagid'],'id,pid,name,md5name,cover,covertime,ordernum,status,addtime');
+                $tagInfo = $tagInfoObj->get_info("id = ".$v['tagid'],'id,pid,name,cover,covertime,ordernum');
                 $tagInfo['cover'] = $aliossObj->getImageUrlNg($aliossObj->IMAGE_TYPE_TAG, $tagInfo['cover'], 460, $tagInfo['covertime']);
                 unset($tagInfo['covertime']);
                 $tagInfoList[] = $tagInfo;
